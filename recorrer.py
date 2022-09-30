0fa6ae46ae9b05c0695bce35ea85f623d9e9f232
Diego Zapata <dzapata1011@gmail.com>
2022-09-29 20:05:46 -0500

    Uso del ciclo for para recorrer una frase o una palabra

---
+++ recorrer.py
@@ -0,0 +1,16 @@
+# def run():
+#     ## Este ejemplo imprime cada caracter de tu nombre
+#     nombre = input("Escribe tu nombre :")
+#     for letra in nombre:
+#         print(letra)
+
+# if __name__ == "__main__":
+#     run()
+
+def run():
+    frase = input("Escribe una frase: ")
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ == "__main__":
+    run()
